9bc00716bb15ee2be8b2b034f3b11a244d504c48
benjamin-arfi <you@example.com>
2023-05-09 16:29:37 +0300

    jour 01

--- jour01/somme.java
+++ jour01/somme.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 public class somme {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Entrez nombre 1 : ");
+            String input = scanner.nextLine();
+            // Convertir la chaîne de caractères en un entier
+            int nombre1 = Integer.parseInt(input);
 
-        System.out.print("Entrez nombre 1 : ");
-        String input = scanner.nextLine();
-        // Convertir la chaîne de caractères en un entier
-        int nombre1 = Integer.parseInt(input);
+            System.out.print("Entrez nombre 2 : ");
+            String ui = scanner.nextLine();
+            // Convertir la chaîne de caractères en un entier
+            int nombre2 = Integer.parseInt(ui);
 
-        System.out.print("Entrez nombre 2 : ");
-        String ui = scanner.nextLine();
-        // Convertir la chaîne de caractères en un entier
-        int nombre2 = Integer.parseInt(ui);
-
-        int resultat = nombre1+ nombre2;
-        System.out.println("le resultat est : " + resultat);
+            int resultat = nombre1+ nombre2;
+            System.out.println("le resultat est : " + resultat);
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
 
     }
