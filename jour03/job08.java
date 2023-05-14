e1f9723a22bd5dfab32543df1c4b1d852c9b9f01
benjamin-arfi <you@example.com>
2023-05-14 16:35:45 +0300

    trie du tableau

--- jour03/job08.java
+++ jour03/job08.java
@@ -1,35 +1,41 @@
+
 import java.util.Arrays;
 public class job08 {
-    public static void main(String[] args) {
-        int [][] tableau = {{5,9,3},{7,2,8},{1,6,4}};
-        for (int i = 0; i < tableau.length; i++) {
-                Arrays.sort(tableau[i]);
-        }
-      
+  public static void main(String[] args) {
+      // Déclaration de la matrice initiale
+int[][] tableau = {{5,9,3},{7,2,8},{1,6,4}};
 
-        // 
-        for(int i=0; i<tableau.length; i++){
-            for(int j=i+1; j<tableau.length; j++){
-              int temp = tableau[i][j];
-              System.out.println(tableau[i][j-1]);
-              System.out.println(tableau[j][i]);
-              tableau[i][j] = tableau[j][i];
-              tableau[j][i] = temp;
-              System.out.println("################");
-            }
-        }
-          
-          // Trier les éléments de chaque ligne
-        for(int i=0; i<tableau.length; i++){
-            Arrays.sort(tableau[i]);
-          }
-        
-          
-         for (int r = 0; r < tableau.length; r++) {
-            for (int ind = 0; ind < tableau.length; ind++) {
-               System.out.println(tableau[r][ind] + " ");
-            }
-            System.out.println();
-         }
+// Récupération des dimensions de la matrice
+int n = tableau.length;
+int m = tableau[0].length;
+
+// Création d'un tableau à une dimension pour stocker tous les éléments de la matrice
+int[] tab1D = new int[n * m];
+int k = 0;
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        tab1D[k++] = tableau[i][j];
     }
+}
+
+// Tri du tableau à une dimension
+Arrays.sort(tab1D);
+
+// Recréation d'une matrice triée à partir du tableau trié
+int[][] matriceTrie = new int[n][m];
+k = 0;
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        matriceTrie[i][j] = tab1D[k++];
+    }
+}
+
+// Affichage de la matrice triée
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        System.out.print(matriceTrie[i][j] + " ");
+    }
+    System.out.println();
+}
+  }
 }
